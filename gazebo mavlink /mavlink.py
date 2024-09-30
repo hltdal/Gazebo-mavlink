aa18d2b4b299458f49bd5eff7f130588df9b5582
hltdal <dalha21@itu.edu.tr>
2024-09-30 13:14:10 +0300

    Refactor GUI code for drone control interface and add emergency control functionality

--- gazebo mavlink /mavlink.py
+++ gazebo mavlink /mavlink.py
@@ -3,14 +3,13 @@ import sys
 import subprocess
 import time
 import threading
-import rospy
 from nav_msgs.msg import Odometry
 from design import Ui_MainWindow
 from PyQt5.QtWidgets import *
 from geometry_msgs.msg import PoseStamped
 # Import mavutil
 from pymavlink import mavutil
-
+import math
 
 
 class LauncherAppFunctions(QMainWindow):
@@ -18,7 +17,6 @@ class LauncherAppFunctions(QMainWindow):
         super().__init__()
         self.main = Ui_MainWindow()
         self.main.setupUi(self)
-        # Dizini ayarla
         self.working_directory = "~"
         
         # Butonlara tıklama olaylarını bağla
@@ -30,6 +28,7 @@ class LauncherAppFunctions(QMainWindow):
         self.main.land_button.clicked.connect(self.land_function)
         self.main.disarm_button.clicked.connect(self.disarm_function)
         self.main.swarm_move_button.clicked.connect(self.swarm_move_function)
+        self.main.emergency_button.clicked.connect(self.emergency_function)
 
     def launch_simulation(self):
         command = "roslaunch iq_sim multi_drone.launch"
@@ -48,9 +47,13 @@ class LauncherAppFunctions(QMainWindow):
         self.drone3.wait_heartbeat()
         print("Drone3 bağlandı")
         self.continually_update_position()
+        #self.continually_check_drone_state()
 
     def arm_function(self):
         try:
+            for drone in [self.drone1, self.drone2, self.drone3]:
+                drone.set_mode("GUIDED")  # GUIDED modunu seç
+                print("Drone GUIDED moduna geçiyor...")
             for drone in [self.drone1, self.drone2, self.drone3]:
                 # Arm the vehicle
                 drone.mav.command_long_send(
@@ -60,16 +63,14 @@ class LauncherAppFunctions(QMainWindow):
                     0,
                     1, 0, 0, 0, 0, 0, 0)
                 # wait until arming confirmed
-                drone.motors_armed_wait()
-                print('Drone armed!')
-            for drone in [self.drone1, self.drone2, self.drone3]:
-                drone.set_mode("GUIDED")  # GUIDED modunu seç
-                print("Drone GUIDED moduna geçiyor...")
+                #drone.motors_armed_wait()
+                #print('Drone armed!')
         except Exception as e:
             print(f"Komut çalıştırılırken bir hata oluştu: {e}")
     
     def get_takeoff_text(self):
-        self.takeoff_value=self.main.takeoff_lineEdit.text()
+        self.takeoff_value=self.main.takeoff_altitude_lineEdit.text()
+        #self.takeoff_velocity=self.main.takeoff_velocity_lineEdit.text()
 
     def take_off_function(self):
         try:
@@ -85,30 +86,60 @@ class LauncherAppFunctions(QMainWindow):
                     0, 0, altitude  # Latitude, Longitude (boş geç), altitude (hedef irtifa)
                 )
                 print(f"Drone {altitude} metreye kalkıyor...")
-        
         except Exception as e:
             print(f"Komut çalıştırılırken bir hata oluştu: {e}")
 
+    def check_drone_state(self):
+        while True:
+            for i, drone in enumerate([self.drone1, self.drone2, self.drone3], 1):
+                print(f"{i}. Drone durumu kontrol ediliyor...")
+                # Her türlü mesajı bekle
+                msg = drone.recv_match(type='HEARTBEAT', blocking=True)
+                if msg:
+                    base_mode = msg.base_mode
+                    # MAV_MODE_FLAG_SAFETY_ARMED = 128 (0x80) -> Silahlanmış mı kontrol et
+                    if base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED:
+                        print("Drone şu an silahlanmış durumda.")
+                        # Havadaki durumu kontrol etmek için diğer modlara da bakabilirsiniz
+                        if base_mode & mavutil.mavlink.MAV_MODE_FLAG_GUIDED_ENABLED:
+                            print("Drone 'Guided' modunda ve muhtemelen havada.")
+                        else:
+                            print("Drone 'Guided' modunda değil.")
+                    else:
+                        print("Drone şu an yerde veya silahlanmamış.")
+                else:
+                    print(f"{i}. Drone'dan mesaj alınmadı.")
+            time.sleep(1)  # 1 saniye bekle
+
     def swarm_move_function(self):
         try:
-            # Drone'ların mevcut pozisyonunu al ve 5 metre ileri hedefini ayarla
-            lat_change = 5 / 111320 # 5 metre ileri hareket
+            # Drone'ların mevcut pozisyonunu al ve 100 metre ileri hedefini ayarla
+            self.move_position_x_value = int(self.main.move_position_x_lineEdit.text())
+            self.move_position_y_value = int(self.main.move_position_y_lineEdit.text())
+            self.move_position_z_value = int(self.main.move_position_z_lineEdit.text())
+
+            self.move_velocity_value = int(self.main.move_velocity_lineEdit.text())
 
             x1, y1, z1 = self.drone1_lat, self.drone1_lon, self.drone1_alt
-            target_x1 = x1 + lat_change
-            self.send_position_target(self.drone1, target_x1, y1, z1)
+            target_x1 = x1 + (self.move_position_x_value / 6378137.0) * (180 / math.pi)
+            target_y1 = y1 + (self.move_position_y_value / 6378137.0) * (180 / math.pi)
+            target_z1 = z1 + self.move_position_z_value 
+            self.send_position_target(self.drone1, target_x1, target_y1, target_z1,self.move_velocity_value)
 
             x2, y2, z2 = self.drone2_lat, self.drone2_lon, self.drone2_alt
-            target_x2 = x2 + lat_change
-            self.send_position_target(self.drone2, target_x2, y2, z2)
+            target_x2 = x2 + (self.move_position_x_value / 6378137.0) * (180 / math.pi)
+            target_y2 = y2 + (self.move_position_y_value / 6378137.0) * (180 / math.pi)
+            target_z2 = z2 + self.move_position_z_value 
+            self.send_position_target(self.drone2, target_x2, target_y2, target_z2,self.move_velocity_value)
 
             x3, y3, z3 = self.drone3_lat, self.drone3_lon, self.drone3_alt
-            target_x3 = x3 + lat_change
-            self.send_position_target(self.drone3, target_x3, y3, z3)
-
+            target_x3 = x3 + (self.move_position_x_value / 6378137.0) * (180 / math.pi)
+            target_y3 = y3 + (self.move_position_y_value / 6378137.0) * (180 / math.pi)
+            target_z3 = z3 + self.move_position_z_value 
+            self.send_position_target(self.drone3, target_x3, target_y3, target_z3,self.move_velocity_value)
         except Exception as e:
             print(f"Drone'lar ileri hareket ettirilirken bir hata oluştu: {e}")
-   
+
     def land_function(self):
         try:
             for drone in [self.drone1, self.drone2, self.drone3]:
@@ -142,26 +173,60 @@ class LauncherAppFunctions(QMainWindow):
         except Exception as e:
             print(f"Komut çalıştırılırken bir hata oluştu: {e}")
     
+    def emergency_function(self):
+        for drone in [self.drone1, self.drone2, self.drone3]:
+            # BRAKE moduna geçiş yaparak drone'u acilen durdur
+            drone.mav.set_mode_send(
+                drone.target_system,
+                mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+                drone.mode_mapping()['BRAKE']
+            )
+            print("Acil durdurma komutu gönderildi: BRAKE moduna geçiliyor.")
+
     def continually_update_position(self):
         # Drone pozisyonlarını güncellemek için iş parçacıkları oluştur
         thread1 = threading.Thread(target=self.update_position_drone1)
         thread2 = threading.Thread(target=self.update_position_drone2)
         thread3 = threading.Thread(target=self.update_position_drone3)
+        thread4 = threading.Thread(target=self.check_drone_state)
+        thread5 = threading.Thread(target=self.get_velocity_and_acceleration)
 
         # İş parçacıklarını başlat
         thread1.start()
         thread2.start()
         thread3.start()
+        thread4.start()
+        thread5.start()
+
+    def get_velocity_and_acceleration(self):
+        while True:
+            for drone in [self.drone1, self.drone2, self.drone3]:
+                # LOCAL_POSITION_NED mesajını al
+                msg = drone.recv_match(type='LOCAL_POSITION_NED', blocking=True)
+                if msg is not None:
+                    vx = msg.vx  # X eksenindeki hız (cm/s, metre/s'ye çevir)
+                    vy = msg.vy  # Y eksenindeki hız
+                    vz = msg.vz  # Z eksenindeki hız
+                    print(f"Velocity X: {vx} m/s, Y: {vy} m/s, Z: {vz} m/s")
+            time.sleep(0.1)  # 0.1 saniye bekle
 
     def update_position_drone1(self):
         try:
             while True:
+                """1. Drone için attitude bilgisi al
+                msg=self.drone1.recv_match(type='ATTITUDE',blocking=True)
+                if msg:
+                    self.drone1_roll = msg.roll
+                    self.drone1_pitch = msg.pitch
+                    self.drone1_yaw = msg.yaw
+                    print(f"1. Drone Attitude: \nRoll={self.drone1_roll}, \nPitch={self.drone1_pitch}, \nYaw={self.drone1_yaw}")
+                """
                 # 1. Drone için pozisyon bilgisi al
                 msg = self.drone1.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
                 if msg:
                     self.drone1_lat = msg.lat / 1e7  # Enlem
                     self.drone1_lon = msg.lon / 1e7  # Boylam
-                    self.drone1_alt = msg.alt / 1000  # Yükseklik (metre cinsinden)
+                    self.drone1_alt = msg.relative_alt / 1000  # Yükseklik (metre cinsinden)
                     
                     # PyQt5 arayüzündeki etiketi güncelle
                     self.main.drone1_position_label.setText(f"1. Drone Pozisyonu: \nLatitude={self.drone1_lat}, \nLongitude={self.drone1_lon}, \nAltitude={self.drone1_alt} m")
@@ -177,7 +242,7 @@ class LauncherAppFunctions(QMainWindow):
                 if msg:
                     self.drone2_lat = msg.lat / 1e7  # Enlem
                     self.drone2_lon = msg.lon / 1e7  # Boylam
-                    self.drone2_alt = msg.alt / 1000  # Yükseklik (metre cinsinden)
+                    self.drone2_alt = msg.relative_alt / 1000  # Yükseklik (metre cinsinden)
                     
                     # PyQt5 arayüzündeki etiketi güncelle
                     self.main.drone2_position_label.setText(f"2. Drone Pozisyonu: \nLatitude={self.drone2_lat}, \nLongitude={self.drone2_lon}, \nAltitude={self.drone2_alt} m")
@@ -193,7 +258,7 @@ class LauncherAppFunctions(QMainWindow):
                 if msg:
                     self.drone3_lat = msg.lat / 1e7  # Enlem
                     self.drone3_lon = msg.lon / 1e7  # Boylam
-                    self.drone3_alt = msg.alt / 1000  # Yükseklik (metre cinsinden)
+                    self.drone3_alt = msg.relative_alt / 1000  # Yükseklik (metre cinsinden)
                     
                     # PyQt5 arayüzündeki etiketi güncelle
                     self.main.drone3_position_label.setText(f"3. Drone Pozisyonu: \nLatitude={self.drone3_lat}, \nLongitude={self.drone3_lon}, \nAltitude={self.drone3_alt} m")
@@ -201,17 +266,23 @@ class LauncherAppFunctions(QMainWindow):
         except Exception as e:
             print(f"Drone3 konumu alınırken hata oluştu: {e}")
 
-    def send_position_target(self, drone, lat, lon, alt):
+    def send_position_target(self, drone, lat, lon, alt,v):
         try:
-            drone.mav.set_position_target_global_int_send(
-                0,  # time_boot_ms (ignored)
-                drone.target_system, drone.target_component,
-                mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT,  # Hedef çerçeve (GLOBAL_RELATIVE_ALT)
-            int(0b110111111000),  # Pozisyon alanlarını kullan
-            int(lat * 1e7), int(lon * 1e7), alt,  # Latitude, Longitude, Altitude
-            0, 0, 0,  # X, Y, Z hızları (ignor)
-            0, 0, 0,  # X, Y, Z ivmeleri (ignor)
-            0, 0  # yaw, yaw_rate (ignor)
+            # Hedef pozisyon ve hız komutu gönderme
+            drone.mav.send(
+                drone.mav.set_position_target_global_int_encode(
+                    0,  # time_boot_ms (geçerli zaman)
+                    #drone.target_system,  # hedef sistem
+                    0,
+                    #drone.target_component,  # hedef komponent
+                    0,
+                    mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT_INT,  # Küresel çerçeve (irtifa göreceli)
+                    0b0000111111111000,
+                    int(lat*1e7), int(lon*1e7), alt,  # Hedef enlem (10^7 ölçeğinde), boylam (10^7 ölçeğinde) ve irtifa (m)
+                    v, v, v,  # Hedef hız (X, Y, Z eksenlerinde m/s cinsinden)
+                    0, 0, 0,  # Hızlanma hedefi yok
+                    0, 0  # Yaw ve Yaw hız hedefi yok
+                )
             )
         except Exception as e:
             print(f"Pozisyon gönderilirken bir hata oluştu: {e}")
